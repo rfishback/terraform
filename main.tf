8b5fd2b273622d44870e0465ead87b33a51fde47
rfishback <54325970+rfishback@users.noreply.github.com>
2020-02-14 16:26:23 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" { 
+  profile = "ec-infra"
+  region  = "us-east-2"
+  assume_role { 
+    role_arn     = var.admin_iam_roles
+    session_name = "Terraform_Session" 
+    external_id  = "Terraform_Session" 
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "terraform-rfishback-test2"
+    # Enable versioning so we can see the full revision history of our
+    # state files
+    versioning {
+      enabled = true
+    }
+    # Enable server-side encryption by default
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+  }
+
+  resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks2"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
